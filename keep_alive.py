b9bb53ec312f94f686f1de8efea75e8f1372539e
OMRANOMO <mranalmsry198@gmail.com>
2025-09-12 21:47:55 +0300

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Bot is running via Webhook!"
+    return "✅ Bot is alive and running via Webhook!"
 
 def run():
-    app.run(host='0.0.0.0', port=5000)
-
+    port = int(os.environ.get("PORT", 5000))  # Render يمرر PORT تلقائيًا
+    app.run(host='0.0.0.0', port=port)
