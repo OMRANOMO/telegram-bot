143a21583944fbc6db762746d466e15e03d2a610
OMRANOMO <mranalmsry198@gmail.com>
2025-09-12 21:51:52 +0300

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 def home():
     return "✅ Bot is alive and running via Webhook!"
 
-def run():
-    port = int(os.environ.get("PORT", 5000))  # Render يمرر PORT تلقائيًا
-    app.run(host='0.0.0.0', port=port)
+# def run():
+ #   port = int(os.environ.get("PORT", 5000))  # Render يمرر PORT تلقائيًا
+  #  app.run(host='0.0.0.0', port=port)
+
